60129073051196913fa6aacb56e9427606fc9b71
Carlos Alonso <carlosalonsomunoz_@hotmail.com>
2022-02-14 22:35:43 +0100

    Prueba suma

--- EntornosAE2/src/principal/Main.java
+++ EntornosAE2/src/principal/Main.java
@@ -6,7 +6,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Prueba Suma
-		
+		Suma suma = new Suma();
+		System.out.println("Vamos a sumar dos reales, el 2.8 y el 17.4");
+		System.out.println("El resultado es: " + suma.dosReales(2.8f, 17.4f));
+		System.out.println("Ahora sumaremos dos enteros: el 60 y el 90");
+		System.out.println("El resultado es: " + suma.dosEnteros(60, 90));
+		System.out.println("También hay que sumar tres reales: 53.24, 36.54 y 75.15");
+		System.out.println("El resultado es: " + suma.tresReales(53.24f, 36.54f, 75.15f));
+		System.out.println("Por último probaremos a acumular números : " + suma.sumaAcumulado(6));
+		System.out.println("Por último probaremos a acumular números : " + suma.sumaAcumulado(9));
+		System.out.println("Por último probaremos a acumular números : " + suma.sumaAcumulado(20));
+		System.out.println("==============================================================");
 		//Prueba Resta
 		
 		//Prueba Producto
@@ -18,7 +28,7 @@ public class Main {
 		System.out.println("También hay que multiplicar tres reales: 23.21, 89.74 y 61.285");
 		System.out.println("El resultado es: " + productos.tresReales(23.21f, 89.74f, 61.285f));
 		System.out.println("Por último probaremos a hacer la potencia al cubo de 36 : " + productos.potencia(36, 3));
-		
+		System.out.println("==============================================================");
 		//Prueba del Cociente
 		Cociente divisiones = new Cociente();
 		System.out.println("Vamos a dividir dos reales, el 53.9832 entre el 4.3232");
@@ -28,7 +38,7 @@ public class Main {
 		System.out.println("Ahora obtendremos el número inverso de 395.57: " + divisiones.inversoReal(395.57f));
 		System.out.println("Por último vamos a obtener la raíz cuadrada de 85.2");
 		System.out.println("El resultado es: " + divisiones.raiz(85.2));
-		
+		System.out.println("==============================================================");
 
 	}
 
